e2fa200cf78594deab70fb645a9eed2c544b0afe
Gonzalo Uribarri <ABC@tutu.com>
2017-03-12 02:45:58 +0330

    Just work on genetis

--- Genetics.py
+++ Genetics.py
@@ -1,1 +1,6 @@
+#!/usr/bin/python
+
 # Genetic part of Genetic Bicycle project
+import numpy as np
+import scipy as sp
+
--- gene.py
+++ gene.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python
 
+# Is a class that contain all the information of a bicycle gene (parameters that can evolve)
+
 class Gene(object):
 	"""Station class"""
+     def __init__(self, M1_x, M1_y, M2_x, M2_y, W1_x, W1_y, W2_x, W2_y, SC, d):
+         self.M1_x = M1_x
+         self.M1_y = M1_y  
+         self.M2_x = M2_x  
+         self.M2_y = M2_y
+         self.W1_x = W1_x
+         self.W1_y = W1_y
+         self.W2_x = W2_x
+         self.W2_y = W2_y
+         self.SC = SC   
+         self.d = d  
+         
+         
+# This function take as an imput the User Settings (another class of the program)
+# that contain the information of the range in wich genes are considered Good Genes 
 
-	def __init__(self, M1_x, M1_y, M2_x, M2_y, W1_x, W1_y, W2_x, W2_y, ):
-		self.name = sname # station name
-		self.stock = inistock #represents how many packages are there in a station
-		self.trainlist = initrainlist
+    def is_good(self,UP):
 
-	def s_load(self):
-		for trs in self.trainlist:
-			for i,pck in enumerate(self.stock):
-				if trs.isfull():
-					break
-				elif trs.destination==pck.destStation:
-					trs.t_load(self.stock[i])
-					del self.stock[i]
+        """Gene check Function"""
 
-
-	def s_unload(self):
-		for trs in self.trainlist:
-			self.stock.append(trs.tstock)
-			trs.t_unload()
-
-	def checkTrains(self):
-		return len(self.trainlist)
-
-	def checkStock(self):
-		return len(self.stock)
-
-	def deliverstock(self,merc):
-		self.stock.append(merc) # merc: Merchandise; 
-
-	def delivertrain(self,trs):
-		self.trainlist.append(trs)
+        x_check = (x_min < self.M1_x < UP.x_max ) and (x_min < self.M2_x < UP.x_max ) and (x_min < self.W1_x < UP.x_max )and (x_min < self.W2_x < UP.x_max) 
+        y_check = (y_min < self.M1_y < UP.y_max ) and (y_min < self.M2_y < UP.y_max ) and (y_min < self.W1_y < UP.y_max )and (y_min < self.W2_y < UP.y_max) 
+        if (x_check and y_check):
+            return 1
+        else:
+            return 0
