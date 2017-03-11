dac2b4984196ac973c0840ed0f27cb4b0e015fb1
Gonzalo Uribarri <ABC@tutu.com>
2017-03-12 02:49:33 +0330

    Nothing done

--- Genetics.py
+++ Genetics.py
@@ -3,4 +3,15 @@
 # Genetic part of Genetic Bicycle project
 import numpy as np
 import scipy as sp
+from gene import Gene
+from user_parameters import UserParameters
 
+# Defines a function that creates a list of n random genes
+#   Those genes are restrected some ranges defined by the user
+def Create_genelist(num,UP):
+    GL=[]
+    g = Gene()
+    for i in range(num):
+        
+            
+    return GL
