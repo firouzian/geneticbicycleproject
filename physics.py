8c8b6cb9b85b906da1747087bad078d249ddbda3
MANZOOR <kazem.manzoor@gmail.com>
2017-03-13 05:30:22 +0330

    it's oK

--- physics.py
+++ physics.py
@@ -2,12 +2,12 @@
 # par1[mass,x,y,r,v,a,f]
 # par2[mass,x,v,r,v,a,f]
 # w1[mass,x,y,r,v,a,f]
-# w2[mass,x,y,v,r,a,f]
+# w2[mass,x,y,r,v,a,f]
 #and for par1,2 the r is zero
 
 ################## start by free fall ############
 import numpy as np 
-
+import math
 
 par1=[1,2,55,4,0,6,7]
 par2=[1,18,56,2,5,6,7]
@@ -35,15 +35,15 @@ print par2
 print W1
 print W2
 
-########################### secend compute the evolotion##############
+########################### secend compute the evolotion ##############
 
-while (par1[2]!=0 or par2[2]!=0):
+#while (par1[2]!=0 or par2[2]!=0):
     
-    i=0
+ #   i=0
     
-    fdpar1=(0,sqrt((par1[1]-par2[1])**2+(par1[2]-par2[2])**2),sqrt((par1[1]-W1[1])**2+(par1[2]-W1[2])**2),sqrt((par1[1]-W2[1])**2+(par1[2]-W2[2])**2))
+fdpar1=(0,math.sqrt(((par1[1]-par2[1])**2)+((par1[2]-par2[2])**2)),math.sqrt(((par1[1]-W1[1])**2)+((par1[2]-W1[2])**2)),math.sqrt(((par1[1]-W2[1])**2)+((par1[2]-W2[2])**2)))
 
-fdpar2=(sqrt((par1[1]-par2[1])**2+(par1[2]-par2[2])**2),0,sqrt((par2[1]-W1[1])**2+(par2[2]-W1[2])**2),sqrt((par2[1]-W2[1])**2+(par2[2]-W2[2])**2))
+fdpar2=(math.sqrt(((par1[1]-par2[1])**2)+((par1[2]-par2[2])**2)),0,math.sqrt(((par2[1]-W1[1])**2)+((par2[2]-W1[2])**2)),math.sqrt((par2[1]-W2[1])**2+(par2[2]-W2[2])**2))
 
 print fdpar1
 
