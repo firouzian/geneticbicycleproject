d531d79ad3dcbbf758d49209853eb14c2c3539a0
moradbiagooi <m.biagooi@gmail.com>
2017-03-15 05:10:45 +0330

    The physics is almost completed. not merged yet

--- allobjects.py
+++ allobjects.py
@@ -1,14 +1,13 @@
 import numpy as np
 import itertools as it
 import wheel 
-#import ground
 
 
 #in this class define all object needed: wheel- particle-spring and ground
 # at this class define update the position and velocity and claculate force
 
 class AllObjects(object):
-    def __init__(self):
+    def __init__(self,ground=[(-10.0,5.0),(10.0,0.0),(50.0,5.0)]):
         print "AllObjects"
         self.wheel_list = []
         self.spring_list = {}
@@ -16,78 +15,147 @@ class AllObjects(object):
         self.spring_const = 1.0
         self.young_const = 1.0
         self.dissip_const = 0.0
+        self.gr_dissip_const = 0.0
         self.gravity = 10.
-#        w1 = wheel.Wheel()
-#        self.wheel_list.append(w1)
+        self.ground_tuple = ground     # list of tuples
+        self.ground_array = []   # list of numpy array
+        self.ground_parallel = [] # vector parallel to each section of the ground
+        self.ground_normal = []
+        self.ground_hit = False
         
     def update(self):
         self.calculate_forces() 
         self.update_positions()
-       # return None
+
         
     def set_springs(self):
-        for w in list(it.combinations_with_replacement(self.wheel_list,2)):
-            if w[0] != w[1]:
-                i = self.wheel_list.index(w[0])
-                j = self.wheel_list.index(w[1])
-                x0 = np.linalg.norm(w[0].position - w[1].position)
-                self.spring_list[(i,j)] = x0
-                #print self.spring_list
-            
-
+        no_wheels = len (self.wheel_list)
+        for i in range (no_wheels):
+            for j in range (i+1,no_wheels):
+                xi_i = np.linalg.norm(self.wheel_list[i].position - self.wheel_list[j].position)
+                self.spring_list[(i,j)] = xi_i
             
     def calc_spring_force (self,i,j):
             ff = np.array([0.,0.],float)
             w_i = self.wheel_list[i]
             w_j = self.wheel_list[j]
-            direct = w_i.position - w_j.position 
-            direct_dist = np.linalg.norm ( direct )
-            inv_direct_dist=1.0/direct_dist
-            direct_norm = direct*inv_direct_dist
-            xi = direct_dist -   self.spring_list[(i,j)] 
-            force = xi * self.spring_const
-            ff = force * direct_norm
-            w_i.add_force ( -ff )
-            w_j.add_force ( +ff )                  
+            d_p = w_i.position - w_j.position
+            d_v = w_i.velocity - w_j.velocity
+            norm_d_p = np.linalg.norm ( d_p )
+            norm_d_v = np.linalg.norm ( d_v )
+            inv_norm_d_p = 1.0 / norm_d_p
+            dir_vec = d_p * inv_norm_d_p
+            xi = norm_d_p - self.spring_list[(i,j)] 
+            force = xi * self.spring_const - d_v * self.dissip_const
+            force_vec = force * dir_vec
+            w_i.add_force ( -force_vec )
+            w_j.add_force ( +force_vec )                  
     
     def calculate_forces(self):
         for w_i in self.wheel_list:
             w_i.force = 0.
 
-        
-        self.calc_spring_force (0,1)
-        self.calc_spring_force (0,2)
-        self.calc_spring_force (0,3)
-        self.calc_spring_force (1,2)        
-        self.calc_spring_force (1,3)
-        self.calc_spring_force (2,3)
-            
+        no_wheels = len (self.wheel_list)
+        for i in range (no_wheels):
+            for j in range (i+1,no_wheels):
+                self.calc_spring_force (i,j)
+
+        self.calc_gravity_force()
+        self.calc_ground_force()
+#        self.calc_flat_ground_force()
+
+
+
+    def calc_gravity_force (self):
         for w_i in self.wheel_list:
             force_mg = - w_i.mass * self.gravity
+            force_vec = np.array([0.,0.],float)
+            force_vec[1] = force_mg
+            w_i.add_force ( force_vec )
+
+
+    def calc_ground_force (self):
+        gr = self.ground_array
+        gr_len = len(gr)
+        for w_i in self.wheel_list:
+            p_i = w_i.position
+            r_i = w_i.radius
+            for j in range(gr_len - 1):
+                vec1 = p_i - gr[j]
+                vec2 = self.ground_parallel[j]
+                dot_vec1_2 = np.dot (vec1, vec2)
+                vec3 = dot_vec1_2 * vec2
+                p_x = gr[j] + vec3
+                vec4 = self.ground_normal[j]      
+                dot_vec1_4 = np.dot (vec1, vec4)
+
+                p_l = gr[j] - (r_i * vec2)
+                p_r = gr[j+1] + (r_i * vec2)
+
+                dis_x  = np.linalg.norm (p_x - p_l)
+                dis_j0 = np.linalg.norm (gr[j] - p_l)
+                dis_j1 = np.linalg.norm (gr[j+1] - p_l)
+                dis_j2 = np.linalg.norm (p_r - p_l)
+
+                xi = 0
+                if 0.0 < dis_x and dis_x <= dis_j0:
+                    r_ii = np.sqrt((r_i*r_i) - (r_i-dis_x)*(r_i-dis_x))
+                    xi = dot_vec1_4 - r_ii
+                if dis_j0 < dis_x and dis_x <= dis_j1:
+                    xi = dot_vec1_4 - r_i
+                if dis_j1 < dis_x and dis_x <= dis_j2:
+                    r_ii = np.sqrt((r_i*r_i) - (dis_j1-dis_x)*(dis_j1-dis_x))
+                    xi = dot_vec1_4 - r_ii
+
+                if xi < 0:
+                    force = -xi * self.young_const * vec4
+                    if w_i.torque>0:
+                        force += -xi*w_i.torque * vec2
+                    w_i.add_force (force)
+                    if r_i == 0:
+                        self.ground_hit = True
+
+                
+    def calc_flat_ground_force (self):
+        for w_i in self.wheel_list:
             xi = w_i.position[1] - w_i.radius
             xi_dot = w_i.velocity[1]
             force_gr = 0
             if  xi < 0: 
-                force_gr = - xi * 10000 - self.dissip_const*xi_dot
-            ff = np.array([0.,0.],float)
-            ff[1] = force_mg + force_gr
-            w_i.add_force ( ff )
-            #print w_i.force
-            
-            
-       # return None
+                force_gr = - xi * 10000 - self.gr_dissip_const * xi_dot
+                if w_i.radius == 0:
+                    self.ground_hit = True
+            force_vec = np.array([0.,0.],float)
+            force_vec[1] = force_gr
+            w_i.add_force ( force_vec )
+           
+
             
     def update_positions (self):
         for w in self.wheel_list:
             w.integrate (self.dt)
-       # return None 
+
     
-    def add_wheel(self,mass,position,radius):
-        w = wheel.Wheel(mass,position,radius)
+    def add_wheel(self,mass,position,radius=0,torque=0):
+        w = wheel.Wheel(mass,position,radius,torque)
         self.wheel_list.append(w)
         
     def make_ground(self):
-        return None
+        for g in self.ground_tuple:
+            g_a = np.array([g[0],g[1]],float)
+            self.ground_array.append (g_a)
+        gr = self.ground_array
+        for i in range(len(gr) - 1):
+            d_p = gr[i+1] - gr[i]
+            norm_d_p = np.linalg.norm (d_p)
+            d_p = d_p / norm_d_p
+            self.ground_parallel.append (d_p)
+            d_n = np.array([-d_p[1],d_p[0]],float)
+            if d_n[1] < 0:
+                d_n *= -1.0         #makes the normal upward
+            self.ground_normal.append (d_n)
+
+
     
     def set_spring_const(self,sp):
         self.spring_const = sp
@@ -97,7 +165,10 @@ class AllObjects(object):
     
     def set_dissip_const(self,sp):
         self.dissip_const = sp
-        
+
+    def set_gr_dissip_const(self,sp):
+        self.gr_dissip_const = sp       
+
     def print_wheel(self,i):
         print self.wheel_list[i].position
         
--- mainphysics.py
+++ mainphysics.py
@@ -4,75 +4,151 @@ import allobjects as aob
 import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
+import ground
 
 
-plt.axis([-10,10,0,30])
-plt.ion()
-plt.show()
+def run_all (gene, UP):
+
+    t_i = 0.0
+    t_f = 10.0
+    dt  = 0.001
+
+    mass_wheel = 1.0
+    mass_parti = 100.0
+
+    rad1  = 1.0
+    rad2  = 1.0
+
+    torque_1 = 1000.0
+    torque_2 = 1000.0
+
+    young_const   = 10000
+    ground_dissip = 50.0
+    spring_dissip = 5.0
 
 
-t_i = 0.0
-t_f = 10.0
-dt  = 0.001
-g=10
-t_steps = int((t_f-t_i)/dt)
+# ===================
+    the_ground = ground.Create_Ground(UP)
 
-a = aob.AllObjects()
+    a = aob.AllObjects (the_ground)
+
+    spring_const = gene.SC
+
+    pos_wheel_1 = np.array ([gene.W1_x, gene.W1_y],float)
+    pos_wheel_2 = np.array ([gene.W2_x, gene.W2_y],float)
+    pos_parti_1 = np.array ([gene.M1_x, gene.M1_y],float)
+    pos_parti_2 = np.array ([gene.M2_x, gene.M2_y],float)
+
+    a.add_wheel (mass_wheel, pos_wheel_1, rad1, torque_1)
+    a.add_wheel (mass_wheel, pos_wheel_2, rad2, torque_2)
+    a.add_wheel (mass_parti, pos_parti_1)
+    a.add_wheel (mass_parti, pos_parti_2)
+
+    a.set_spring_const (spring_const)
+    a.set_young_const (young_const )
+    a.set_gr_dissip_const (ground_dissip)
+    a.set_dissip_const ( spring_dissip )
+
+    a.make_ground()
+
+    a.set_springs() # assigns the rest distances of strings
+
+    initial_position = a.wheel_list[0].position[0]
+
+    t_steps = int((t_f-t_i)/dt)
+    for i in range (t_steps):
+        a.update()
+        t = t_i+i*dt
+        if a.ground_hit:
+            dist = a.wheel_list[0].position[0] - initial_position
+            return dist, gene
+
+    dist = a.wheel_list[0].position[0] - initial_position
+    return dist, gene
+
+##-------------------------------------
+##-------------------------------------
+##-------------------------------------
+
+
+if __name__ == '__main__':
+
+    plt.axis([-10,10,0,30])
+    plt.ion()
+    plt.show()
+
+
+    t_i = 0.0
+    t_f = 10.0
+    dt  = 0.001
+    g=10
+    t_steps = int((t_f-t_i)/dt)
+
+    a = aob.AllObjects()
 
 #--------
-mass1 = 1.0
-mass2 = 1.0
+    mass1 = 1.0
+    mass2 = 1.0
 
-rad1  = 1.0
-rad2  = 1.0
+    rad1  = 3.0
+    rad2  = 1.0
 
-position1 = np.array ([3.,5.],float)
-position2 = np.array ([4.,9.],float)
-position3 = np.array ([8.,4.],float)
-position4 = np.array ([11.,10.],float)
+    torque1 = 1000.0
 
-a.add_wheel (mass1,position1,rad1)
-a.add_wheel (mass2,position2,rad2)
-a.add_wheel (mass2,position3,rad1)
-a.add_wheel (mass2,position4,rad1)
+    position1 = np.array ([3.,5.],float)
+    position2 = np.array ([4.,9.],float)
+    position3 = np.array ([8.,4.],float)
+    position4 = np.array ([11.,10.],float)
 
-a.set_spring_const (50.0)
-a.set_young_const (10000.0 )
-a.set_dissip_const (50.0)
+    a.add_wheel (mass1,position1,rad1, torque1)
+    a.add_wheel (mass2,position2)
+    a.add_wheel (mass2,position3,rad1, torque1)
+    a.add_wheel (mass2,position4)
+
+    a.set_spring_const (50.0)
+    a.set_young_const (10000.0 )
+    a.set_gr_dissip_const (50.0)
+    a.set_dissip_const (1.1)
 #---------
-a.make_ground()
+    a.make_ground()
 #---------
 
-a.set_springs() # assigns the rest distances of strings
+    a.set_springs() # assigns the rest distances of strings
 
-for i in range (t_steps):
-    a.update()
+    initial_position = a.wheel_list[0].position[0]
+
+    for i in range (t_steps):
+        a.update()
+        if a.ground_hit:
+            print 'ground hit'
+            print a.wheel_list[0].position[0] - initial_position
+            exit()
     
-    t = t_i+i*dt
-    if i%100 == 0:
-        y1 = a.wheel_list[0].position[1]
-        x1 = a.wheel_list[0].position[0]
+        t = t_i+i*dt
+        if i%100 == 0:
+            y1 = a.wheel_list[0].position[1]
+            x1 = a.wheel_list[0].position[0]
         
-        y2 = a.wheel_list[1].position[1]
-        x2 = a.wheel_list[1].position[0]
+            y2 = a.wheel_list[1].position[1]
+            x2 = a.wheel_list[1].position[0]
         
-        y3 = a.wheel_list[2].position[1]
-        x3 = a.wheel_list[2].position[0]
+            y3 = a.wheel_list[2].position[1]
+            x3 = a.wheel_list[2].position[0]
         
-        y4 = a.wheel_list[3].position[1]
-        x4 = a.wheel_list[3].position[0]
-        #plt.scatter(t,y1)
-        #plt.scatter(t,y2)
+            y4 = a.wheel_list[3].position[1]
+            x4 = a.wheel_list[3].position[0]
+            #plt.scatter(t,y1)
+            #plt.scatter(t,y2)
 
-        plt.cla()
-        plt.axis([0.0,20,0,20])
-        plt.scatter(x1,y1)
-        plt.scatter(x2,y2)
-        plt.scatter(x3,y3)
-        plt.scatter(x4,y4)  
+            plt.cla()
+            plt.axis([0.0,50,0,50])
+            plt.scatter(x1,y1)
+            plt.scatter(x2,y2)
+            plt.scatter(x3,y3)
+            plt.scatter(x4,y4)  
                      
-        plt.pause(0.01)
+            plt.pause(0.005)
         
         
         
-plt.show(block=False)
+    plt.show(block=False)
